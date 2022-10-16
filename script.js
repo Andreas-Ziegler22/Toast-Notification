0e19c049ddf223ebae1d84f35774516de3e139bf
MrBurp <andreass.ziegler@gmail.com>
2022-10-16 21:47:57 +0200

    types  css

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ const messages = [
   "Message Four",
 ];
 
-button.addEventListener("click", () => createNotification());
+button.addEventListener("click", () =>
+  createNotification("This is invalid data")
+);
 
-function createNotification() {
+function createNotification(message = null, type = null) {
   const notif = document.createElement("div");
   notif.classList.add("toast");
+  notif.classList.add(type ? type : "info");
 
   notif.innerText = getRandomMessage();
 
